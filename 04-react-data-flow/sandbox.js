6e7c6ff0d87c598b0b8810d5f785c9877abb01da
JawaraGordon <jawaragordon@icloud.com>
2022-05-07 09:29:02 -0400

    array sandbox

---
+++ 04-react-data-flow/sandbox.js
@@ -0,0 +1,13 @@
+console.log('hi');
+
+const newArr = [1, 2, 3, 4];
+
+// const squaredArr = newArr.map((el) => el ** 2);
+
+// const evensArr = newArr.filter((el) => el % 2 === 0);
+
+const findArr = newArr.find((el) => el % 2 === 0);
+
+// const evensArr = squaredArr.filter((el) => el % 2);
+
+console.log(findArr);
